072f116e8a9995a9438dad8c1a21e70d6af56bca
Chanawat Thuasuphap <earthza991@hotmail.com>
2025-05-06 13:23:07 +0700

    test: 💍 add different lengths array case

--- test/merge.test.ts
+++ test/merge.test.ts
@@ -2,9 +2,16 @@ import { merge } from "../src/merge";
 
 describe("merge", () => {
   it("merges three arrays as specified", () => {
-    const c1 = [0, 2, 4];
-    const c2 = [5, 3, 1];
-    const c3 = [-1, 6, 7];
+    const c1: number[] = [0, 2, 4];
+    const c2: number[] = [5, 3, 1];
+    const c3: number[] = [-1, 6, 7];
     expect(merge(c1, c2, c3)).toEqual([-1, 0, 1, 2, 3, 4, 5, 6, 7]);
+  });
+  
+  it("handles arrays of different lengths", () => {
+    const c1: number[] = [];
+    const c2: number[] = [20, 13, 5, 4, 3, 2];
+    const c3: number[] = [6, 7, 8, 9, 10, 11, 14];
+    expect(merge(c1, c2, c3)).toEqual([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 20]);
   });
 });
